ca0cd4d5efd6efde44563f14991a42dab6fdbeea
Yuan Xue <ziyaajiang@gmail.com>
2014-07-16 23:40:07 +0800

    more

--- src/learn_macro/core.clj
+++ src/learn_macro/core.clj
@@ -14,5 +14,39 @@
 
 (eval (list 'def 'xxx 3))
 
-(nil!3 'yyy)
+(nil!3 yyy)
+
+(let [b 3]
+  `(a ~b c))
+
+(let [b 3]
+  `(a (~b c)))
+
+(let [a 1
+      b 2
+      c 3]
+  `(a b ~c ('~(+ a b c)) (+ a b) 'c '((~a ~b))))
+
+
+
+
+
+(defmacro nif
+  [expr pos zero neg]
+  `(case (int (Math/signum (double ~expr)))
+     1 ~pos
+     0 ~zero
+     -1 ~neg))
+
+(nif -3 'p 'z 'n)
+
+(nif (- 2 3) 'p 'z 'n)
+
+(let [b '(1 2 3)]
+  `(a ~@b c))
+
+
+
+
+
 
